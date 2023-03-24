e3fe5bf7021a2980a2fb02c3566e6fcfb6e95dd5
vsa_user <kritik.sova@gmail.com>
2023-03-24 15:12:41 +0300

    Continue

--- Program.cs
+++ Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+MyClass myClass = new MyClass("str1", "str2");
+Console.WriteLine(myClass.Summ());
+Console.ReadLine();
