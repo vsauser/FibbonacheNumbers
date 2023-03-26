5224e1963d383eb1c548152bd48ef63e3775c209
vsa_user <kritik.sova@gmail.com>
2023-03-27 00:59:00 +0300

    Finish

--- MyClass.cs
+++ MyClass.cs
@@ -1,14 +1,27 @@
-public class MyClass
+public class FibbonacheClass
 {
-    string _str1;
-    string _str2;
-    public MyClass(string str1, string str2)
+    public double _a;
+    public double _b;
+    public double _Summ;
+    double _fibbonacheSummStepI;
+    double _fibbonacheBefore;
+    double _fibbonacheAfter;
+
+    public FibbonacheClass()
     {
-        _str1 = str1;
-        _str2 = str2;
+        _a = 0;
+        _b = 1;
+        _fibbonacheBefore = _a;
+        _fibbonacheAfter = _b;
+        _fibbonacheSummStepI = 0;
+        _Summ = _a + _b;
     }
-    public string Summ()
+    public double fibbonacheNumber()
     {
-        return _str1 + " " + _str2;
+            _fibbonacheSummStepI = _fibbonacheBefore + _fibbonacheAfter;
+            _fibbonacheBefore = _fibbonacheAfter;
+            _fibbonacheAfter = _fibbonacheSummStepI;
+            _Summ = _Summ + _fibbonacheAfter;
+            return _fibbonacheAfter;
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,6 +1,37 @@
 // See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello!");
+int _n;
+string _continue = "0";
+double _a;
+double _b;
+double _fibbonacheSumm = 0;
 
-MyClass myClass = new MyClass("str1", "str2");
-Console.WriteLine(myClass.Summ());
+Console.WriteLine("Summator for Fibbonache numbers");
+Console.WriteLine();
+
+do
+{
+    FibbonacheClass fibbonacheClass = new FibbonacheClass();
+    Console.Write("Insert numbers for Fibbonache sequence:");
+
+    _n = Convert.ToInt32(Console.ReadLine());
+    _a = fibbonacheClass._a;
+    _b = fibbonacheClass._b;
+    _fibbonacheSumm = _a + _b;
+
+    Console.WriteLine(_a);
+    Console.WriteLine(_b);
+
+    for(int i = 1; i < (_n - 1); i++)
+    {
+        Console.WriteLine(fibbonacheClass.fibbonacheNumber());
+    }  
+
+    Console.WriteLine("Sum of Fibbonache sequence = " + fibbonacheClass._Summ);
+    Console.WriteLine();
+    Console.WriteLine("If you want to continue - press 1, if not - press any key");
+
+    _continue =Console.ReadLine();
+
+}while(_continue == "1");
+
 Console.ReadLine();
